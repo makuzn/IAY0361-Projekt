c62c97c1382767132065b5f0799e991f2e7489f5
Maria Kuznetsova <kuznetsova.marija@gmail.com>
2017-09-20 15:50:17 +0300

    Init

---
+++ forecast/src/Weather.java
@@ -0,0 +1,17 @@
+public class Weather {
+
+    public void getCurrentWeather()
+    {
+        //
+    }
+
+    public void getThreeDaysWeather()
+    {
+        //
+    }
+
+    public void getWeatherForLocation(Double xxx, Double yyy)
+    {
+        //
+    }
+}
---
+++ forecast/src/WeatherTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherTest {
+    @org.junit.jupiter.api.Test
+    void getCurrentWeather() {
+        assertTrue(false);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getThreeDaysWeather() {
+        assertTrue(false);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWeatherForLocation() {
+        assertTrue(false);
+    }
+
+}
